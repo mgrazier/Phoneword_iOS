ab21fac72ff23845b00628888d9fd3b9e6ceee9b
Mike Grazier <michael.grazier@gmail.com>
2015-08-28 09:32:19 -0500

    Initial Commit

---
+++ Phoneword_iOS/RootViewController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Drawing;
+
+using Foundation;
+using UIKit;
+
+namespace Phoneword_iOS
+{
+    public partial class RootViewController : UIViewController
+    {
+        public RootViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            // Releases the view if it doesn't have a superview.
+            base.DidReceiveMemoryWarning();
+
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        #region View lifecycle
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            // Perform any additional setup after loading the view, typically from a nib.
+
+            string translatedNumber = "";
+            TranslateButton.TouchUpInside += (object sender, EventArgs e) =>
+            {
+                // Convert the phone number with text to a number using PHoneTranslater.cs
+                translatedNumber = PhoneTranslator.ToNumber(PhoneNumberText.Text);
+
+                // Dismiss the keyboard if text field was tapped
+                PhoneNumberText.ResignFirstResponder();
+
+                if (translatedNumber == "") {
+                    CallButton.SetTitle("Call ", UIControlState.Normal);
+                    CallButton.Enabled = false;
+                }
+                else {
+                    CallButton.SetTitle("Call " + translatedNumber, UIControlState.Normal);
+                    CallButton.Enabled = true;
+                }
+            };
+
+            CallButton.TouchUpInside += (object sender, EventArgs e) =>
+            {
+                // Use URL handler with tel: prefix to invoke Apple's Phone app...
+                var url = new NSUrl("tel:" + translatedNumber);
+
+                // ...Otherwise show an alert dialog
+                if (!UIApplication.SharedApplication.OpenUrl(url)) {
+                    var alert = UIAlertController.Create("Not supported", "Scheme 'tel:' is not supported on this device", UIAlertControllerStyle.Alert);
+                    alert.AddAction(UIAlertAction.Create("Ok", UIAlertActionStyle.Default, null));
+                    PresentViewController(alert, true, null);
+                }
+            };
+        }
+
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
+        }
+
+        public override void ViewDidAppear(bool animated)
+        {
+            base.ViewDidAppear(animated);
+        }
+
+        public override void ViewWillDisappear(bool animated)
+        {
+            base.ViewWillDisappear(animated);
+        }
+
+        public override void ViewDidDisappear(bool animated)
+        {
+            base.ViewDidDisappear(animated);
+        }
+
+        #endregion
+    }
+}
